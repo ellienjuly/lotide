92cbbaf658631258343d664b69fb0fa79722cd18
Taeyeong Nam <ellienjuly@gmail.com>
2021-07-26 22:57:34 +0000

    Using mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,9 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+const tail = require('../tail');
+
+describe("#Tail", () => {
+  it("returns true if an two arrays are the same", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]),["Lighthouse", "Labs"])
+  })
+})
