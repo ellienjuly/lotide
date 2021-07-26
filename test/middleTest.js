106206bbdb2948d388cc996b76a7a4dc706ca477
Taeyeong Nam <ellienjuly@gmail.com>
2021-07-26 17:27:53 +0000

    Refactored

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,11 @@
+const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
 console.log(middle([1, 2, 3, 4 ,5 ,6 ,7]));
-console.log(middle([1])); 
+console.log(middle([1]));
+
+
+console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
