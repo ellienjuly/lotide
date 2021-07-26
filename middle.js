388984d7167e174be58f6d3d9e77c997a19a432d
Taeyeong Nam <ellienjuly@gmail.com>
2021-07-26 17:27:04 +0000

    Refactored

--- middle.js
+++ middle.js
@@ -1,28 +1,5 @@
-const assertEqual = function(actual, expected) {
-  let isTrue = actual === expected;
-  if (isTrue) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  } else if (!isTrue) {
-    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  let result;
-
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        result = `✅✅✅ Assertion Passed: True`;
-      } else {
-        result = `🛑🛑🛑 Assertion Failed: False`;
-      }
-    }
-  } else {
-    result = `🛑🛑🛑 Assertion Failed: False`;
-  }
-  return result;
-};
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const middle = function(arr) {
@@ -47,13 +24,5 @@ const middle = function(arr) {
   }
 };
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
-console.log(middle([1, 2, 3, 4 ,5 ,6 ,7]));
-console.log(middle([1]));
-
-
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(assertEqual(middle([1, 2, 3, 4])[0], 2));
-console.log(assertEqual(middle([1, 2, 3, 4])[1], 3));
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const middle = require('../middle');
+
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+console.log(middle([1, 2, 3, 4 ,5 ,6 ,7]));
+console.log(middle([1])); 
